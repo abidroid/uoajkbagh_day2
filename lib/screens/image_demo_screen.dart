30b3792875f92d5e8d179eb71086801db6b66dc5
Abid <abidroid1@gmail.com>
2025-05-01 15:16:45 +0500

    images

--- lib/screens/image_demo_screen.dart
+++ lib/screens/image_demo_screen.dart
@@ -10,13 +10,46 @@ class ImageDemoScreen extends StatelessWidget {
         title: const Text('Image'),
         backgroundColor: Colors.deepOrange,
       ),
-      body: Column(
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.purple,
+              backgroundImage: NetworkImage(
+                'https://avatars.githubusercontent.com/u/17814795?v=4',
+              ),
+              child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Icon(Icons.camera_alt)),
+            ),
 
-          Icon(Icons.abc, size: 100, color: Colors.green,),
-          Icon(Icons.shopping_cart, size: 200, color: Colors.orange,),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image(
+                width: 200,
+                height: 200,
+                image: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/17814795?v=4',
+                ),
+              ),
+            ),
 
-        ],
+            Image(
+              width: 200,
+              height: 200,
+              image: AssetImage('images/abid.png'),
+            ),
+
+            Image.asset('images/abid.png',
+
+            width: 100,height: 100,
+            ),
+
+            Icon(Icons.abc, size: 100, color: Colors.green),
+            Icon(Icons.shopping_cart, size: 200, color: Colors.orange),
+          ],
+        ),
       ),
     );
   }
